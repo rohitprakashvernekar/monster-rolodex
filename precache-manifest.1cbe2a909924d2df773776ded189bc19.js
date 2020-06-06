7f02adc7e0e65ab6583a646db137cc22e4c86782
rohitvernekar7 <rohitvernekar7@yahoo.com>
2020-06-06 11:40:47 +0530

    Updates

---
+++ precache-manifest.1cbe2a909924d2df773776ded189bc19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5be34352ee2f8e976b1157ac9deef0ef",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "419d72d26f0dc1ced3ba",
+    "url": "/monster-rolodex/static/css/main.402ea6ab.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monster-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "419d72d26f0dc1ced3ba",
+    "url": "/monster-rolodex/static/js/main.bae6c433.chunk.js"
+  },
+  {
+    "revision": "b46281a00ff77db46658",
+    "url": "/monster-rolodex/static/js/runtime-main.c3b95803.js"
+  }
+]);
